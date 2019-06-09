6d8a5faa1fde772f41b369bd56619c643942c82a
Lukie Kang <lukiek+f@gmail.com>
2019-06-09 13:22:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ class Song
 
   @@count = 0
   @@artists = []
-  @@all_artists = []
   @@genres = []
   @@artist_count = {}
   @@genre_count = {}
@@ -32,17 +31,6 @@ class Song
     @@genres.uniq
   end
 
-  # def self.artist_count
-  # @@artists.each do | artist |
-  #   if artist_count.key?(artist)
-  #     artist_count["artist"] +=1
-  #   else
-  #     artist_count["artist"] = 1
-  #   end
-  # end # end of artists.each
-  #
-  # @@artist_count
-  # end
 
   def self.genre_count
     @@genres.each do | genre |
@@ -54,6 +42,18 @@ class Song
       end
     end # end of genre.each
     @@genre_count
+  end  # end of method
+  
+  def self.artist_count
+    @@artists.each do | artist |
+      # @@artist_count["#{artist}"] = 1
+      if @@artist_count["#{artist}"]
+        @@artist_count["#{artist}"] += 1
+      else
+        @@artist_count["#{artist}"] = 1
+      end
+    end # end of artist.each
+    @@artist_count
   end  # end of method
 
 
